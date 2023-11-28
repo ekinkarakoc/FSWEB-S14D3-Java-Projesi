787dee8491d61bd11dfd0d1c0919515ba9cd2c8d
ekinkarakoc <ekinkarakoc66@gmail.com>
2023-11-28 21:40:07 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import com.workintech.car.company.Car;
+import com.workintech.car.company.Ford;
+import com.workintech.car.company.Holden;
+import com.workintech.car.company.Mitsubishi;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car(8, "Base car");
+
+        System.out.println(car.startEngine());
+
+        System.out.println(car.accelerate());
+
+        System.out.println(car.brake());
+
+        System.out.println("*****************************");
+
+        Car mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+
+        System.out.println(mitsubishi.startEngine());
+
+        System.out.println(mitsubishi.accelerate());
+
+        System.out.println(mitsubishi.brake());
+
+        System.out.println("*****************************");
+
+
+        Car ford = new Ford(6, "Ford Falcon");
+
+        System.out.println(ford.startEngine());
+
+        System.out.println(ford.accelerate());
+
+        System.out.println(ford.brake());
+
+        System.out.println("*****************************");
+
+
+        Car holden = new Holden(6, "Holden Commodore");
+
+        System.out.println(holden.startEngine());
+
+        System.out.println(holden.accelerate());
+
+        System.out.println(holden.brake());
     }
 }
